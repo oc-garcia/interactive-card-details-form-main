82ead59010fc8e7268041190ae3b2122c33f2ee5
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-23 17:01:22 -0300

    input basic functions finished

--- main.js
+++ main.js
@@ -1,11 +1,59 @@
-const nameInput = document.getElementById("form__name"); 
+// Form
+const nameInput = document.getElementById("form__name");
 const numberInput = document.getElementById("form__number");
 const cardMonthInput = document.getElementById("form__date-month");
 const cardYearInput = document.getElementById("form__date-year");
 const cardCvcInput = document.getElementById("form__cvc");
+
+// Card displayed
 const displayCardNumber = document.getElementById("card__number");
 const displayCardName = document.getElementById("card__name");
 const displayCardMonth = document.getElementById("card__month");
 const displayCardYear = document.getElementById("card__year");
+const displayCardCvc = document.getElementById("card__cvc");
 
+nameInput.addEventListener("input", changeName);
+numberInput.addEventListener("input", changeNumber);
+cardMonthInput.addEventListener("input", changeMonth);
+cardYearInput.addEventListener("input", changeYear);
+cardCvcInput.addEventListener("input", changeCVC);
 
+function changeName() {
+  if ((displayCardName.innerHTML = nameInput.value)) {
+    displayCardName.innerHTML = nameInput.value;
+  } else {
+    displayCardName.innerHTML = "e.g. Jane Appleseed";
+  }
+}
+
+function changeNumber() {
+  if ((displayCardNumber.innerHTML = numberInput.value)) {
+    displayCardNumber.innerHTML = numberInput.value;
+  } else {
+    displayCardNumber.innerHTML = "0000 0000 0000 0000";
+  }
+}
+
+function changeMonth() {
+  if ((displayCardMonth.innerHTML = cardMonthInput.value)) {
+    displayCardMonth.innerHTML = cardMonthInput.value;
+  } else {
+    displayCardMonth.innerHTML = "00";
+  }
+}
+
+function changeYear() {
+  if ((displayCardYear.innerHTML = cardYearInput.value)) {
+    displayCardYear.innerHTML = cardYearInput.value;
+  } else {
+    displayCardYear.innerHTML = "YY";
+  }
+}
+
+function changeCVC() {
+  if (displayCardCvc.innerHTML = cardCvcInput.value) {
+    displayCardCvc.innerHTML = cardCvcInput.value;
+  } else {
+    displayCardCvc.innerHTML = "e.g. 123";
+  }
+}
