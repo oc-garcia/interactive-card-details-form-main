a9334f6f986a29ea81202a3aea1d7a85bc236bca
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-23 16:21:50 -0300

    Starting main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-let nameInput = document.getElementById("") 
+const nameInput = document.getElementById("form__name"); 
+const numberInput = document.getElementById("form__number");
+const cardMonthInput = document.getElementById("form__date-month");
+const cardYearInput = document.getElementById("form__date-year");
+const cardCvcInput = document.getElementById("form__cvc");
+const displayCardNumber = document.getElementById("card__number");
+const displayCardName = document.getElementById("card__name");
+const displayCardMonth = document.getElementById("card__month");
+const displayCardYear = document.getElementById("card__year");
+
+
