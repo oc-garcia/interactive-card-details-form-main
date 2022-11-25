631c3ccc8e3e24f868d9880021571baf2e7cc815
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-25 16:15:37 -0300

    gradient borders

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ function changeCVC() {
     displayCardCvc.innerHTML = cardCvcInput.value;
     document.getElementById("span__cvc").innerHTML = "";
   } else {
-    displayCardCvc.innerHTML = "e.g. 123";
+    displayCardCvc.innerHTML = "000";
     if (cardCvcInput.value == "") {
       document.getElementById("span__cvc").innerHTML = "Cant't be blank";
     } else {
@@ -119,3 +119,31 @@ function submitForm() {
     document.getElementById("thanks__card").className = "end__status";
   }
 }
+
+/*
+function formatCardNumber(value) {
+  // remove all non digit characters
+  var value = value.replace(/\D/g, "");
+  var formattedValue;
+  var maxLength;
+  // american express, 15 digits
+  if (/^3[47]\d{0,13}$/.test(value)) {
+    formattedValue = value.replace(/(\d{4})/, "$1 ").replace(/(\d{4}) (\d{6})/, "$1 $2 ");
+    maxLength = 17;
+  } else if (/^3(?:0[0-5]|[68]\d)\d{0,11}$/.test(value)) {
+    // diner's club, 14 digits
+    formattedValue = value.replace(/(\d{4})/, "$1 ").replace(/(\d{4}) (\d{6})/, "$1 $2 ");
+    maxLength = 16;
+  } else if (/^\d{0,16}$/.test(value)) {
+    // regular cc number, 16 digits
+    formattedValue = value
+      .replace(/(\d{4})/, "$1 ")
+      .replace(/(\d{4}) (\d{4})/, "$1 $2 ")
+      .replace(/(\d{4}) (\d{4}) (\d{4})/, "$1 $2 $3 ");
+    maxLength = 19;
+  }
+
+  $("#cc").attr("maxlength", maxLength);
+  return formattedValue;
+}
+*/
