8117fc7b52c6885d5ccdf77f85d6006c54937b45
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-25 15:10:46 -0300

    javascript improvements

--- main.js
+++ main.js
@@ -18,8 +18,11 @@ nameInput.addEventListener("blur", changeName);
 numberInput.addEventListener("input", changeNumber);
 numberInput.addEventListener("blur", changeNumber);
 cardMonthInput.addEventListener("input", changeMonth);
+cardMonthInput.addEventListener("blur", changeMonth);
 cardYearInput.addEventListener("input", changeYear);
+cardYearInput.addEventListener("blur", changeYear);
 cardCvcInput.addEventListener("input", changeCVC);
+cardCvcInput.addEventListener("blur", changeCVC);
 btnSubmit.addEventListener("click", submitForm);
 
 function changeName() {
@@ -53,28 +56,66 @@ function changeNumber() {
 function changeMonth() {
   if ((displayCardMonth.innerHTML = cardMonthInput.value)) {
     displayCardMonth.innerHTML = cardMonthInput.value;
+    document.getElementById("span__date").innerHTML = "";
   } else {
     displayCardMonth.innerHTML = "00";
+    if (cardMonthInput.value == "") {
+      document.getElementById("span__date").innerHTML = "Cant't be blank";
+    } else {
+      document.getElementById("span__date").innerHTML = "";
+    }
   }
 }
 
 function changeYear() {
   if ((displayCardYear.innerHTML = cardYearInput.value)) {
     displayCardYear.innerHTML = cardYearInput.value;
+    document.getElementById("span__date").innerHTML = "";
   } else {
     displayCardYear.innerHTML = "YY";
+    if (cardYearInput.value == "") {
+      document.getElementById("span__date").innerHTML = "Cant't be blank";
+    } else {
+      document.getElementById("span__date").innerHTML = "";
+    }
   }
 }
 
 function changeCVC() {
   if ((displayCardCvc.innerHTML = cardCvcInput.value)) {
     displayCardCvc.innerHTML = cardCvcInput.value;
+    document.getElementById("span__cvc").innerHTML = "";
   } else {
     displayCardCvc.innerHTML = "e.g. 123";
+    if (cardCvcInput.value == "") {
+      document.getElementById("span__cvc").innerHTML = "Cant't be blank";
+    } else {
+      document.getElementById("span__cvc").innerHTML = "";
+    }
   }
 }
 
 function submitForm() {
-  document.getElementById("form").className = "hide__container";
-  document.getElementById("thanks__card").className = "end__status";
+  if (
+    cardCvcInput.value == "" ||
+    cardYearInput.value == "" ||
+    cardMonthInput.value == "" ||
+    numberInput.value == "" ||
+    nameInput.value == ""
+  ) {
+    if (nameInput.value == "") {
+      changeName();
+    } else if (numberInput.value == "") {
+      changeNumber();
+    } else if (cardMonthInput.value == "") {
+      changeMonth();
+    } else if (cardYearInput.value == "") {
+      changeYear();
+    } else if (cardCvcInput.value == "") {
+      changeCVC();
+    }
+  } else {
+    document.getElementById("form").className = "hide__container";
+    document.getElementById("thanks__card").className = "end__status";
+  }
 }
